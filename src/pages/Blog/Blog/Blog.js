04b5db7e552a3e537110abaceeb6626494013b59
Md Rahi <fhrfhr102030@gmail.com>
2022-10-27 05:40:28 +0600

    blog page design done

--- src/pages/Blog/Blog/Blog.js
+++ src/pages/Blog/Blog/Blog.js
@@ -1,9 +1,48 @@
 import React from 'react';
+import Accordion from 'react-bootstrap/Accordion';
 
 const Blog = () => {
     return (
         <div>
-            <h1>Blog</h1>
+            <Accordion defaultActiveKey="0">
+                <Accordion.Item eventKey="0">
+                    <Accordion.Header>What is cors?</Accordion.Header>
+                    <Accordion.Body>
+                        Cross-origin resource sharing (CORS) is a browser security feature that restricts cross-origin HTTP requests that are initiated from scripts running in the browser. If your REST API's resources receive non-simple cross-origin HTTP requests, you need to enable CORS support.
+                    </Accordion.Body>
+                </Accordion.Item>
+
+                <Accordion.Item eventKey="1">
+                    <Accordion.Header>Why are you using firebase? What other options do you have to implement authentication?</Accordion.Header>
+                    <Accordion.Body>
+                        Firebase Authentication provides backend services, easy-to-use SDKs, and ready-made UI libraries to authenticate users to your app. It supports authentication using passwords, phone numbers, popular federated identity providers like Google, Facebook and Twitter, and more.
+                        <h5 className='mt-3'>Other options are:</h5>
+                        <ul>
+                            <li>Single-Factor/Primary AuthenticationSingle-Factor/Primary Authentication</li>
+                            <li>Two-Factor Authentication (2FA)</li>
+                            <li>Single Sign-On (SSO)</li>
+                            <li>Multi-Factor Authentication (MFA)</li>
+                            <li>Password Authentication Protocol (PAP)</li>
+                            <li>Challenge Handshake Authentication Protocol (CHAP)</li>
+                            <li>Extensible Authentication Protocol (EAP)</li>
+                        </ul>
+                    </Accordion.Body>
+                </Accordion.Item>
+
+                <Accordion.Item eventKey="2">
+                    <Accordion.Header>How does the private route work?</Accordion.Header>
+                    <Accordion.Body>
+                        The private route component is similar to the public route, the only change is that redirect URL and authenticate condition. If the user is not authenticated he will be redirected to the login page and the user can only access the authenticated routes If he is authenticated (Logged in).
+                    </Accordion.Body>
+                </Accordion.Item>
+
+                <Accordion.Item eventKey="3">
+                    <Accordion.Header>What is Node? How does Node work?</Accordion.Header>
+                    <Accordion.Body>
+                        Node. js is a JavaScript runtime environment that achieves low latency and high throughput by taking a “non-blocking” approach to serving requests. It is a used as backend service where javascript works on the server-side of the application. This way javascript is used on both frontend and backend. Node. js runs on chrome v8 engine which converts javascript code into machine code, it is highly scalable, lightweight, fast, and data-intensive. 
+                    </Accordion.Body>
+                </Accordion.Item>
+            </Accordion>  
         </div>
     );
 };
